61df9b7743a8f840657613de933c7799982ab636
hollywoodzzz <kovalioff28@gmail.com>
2022-06-15 08:26:26 +0300

    bubble sort added

---
+++ bubbleSort.js
@@ -0,0 +1,21 @@
+var unsortedArray = [4,3,2,1]
+console.log(unsortedArray);
+
+function bubbleSort(arr){
+  let swapped = true;
+  do{
+    swapped = false;
+    for(i = 0; i< arr.length; i++){
+      if(arr[i] > arr[i+1]){
+        let temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+        swapped = true;
+        console.log(arr)
+      }
+    }
+  } while(swapped)
+  return arr;
+}
+
+bubbleSort(unsortedArray)
