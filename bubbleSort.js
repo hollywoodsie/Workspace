38708b54bcf3f9d6cb16eaa74f2efba4e350514d
hollywoodzzz <kovalioff28@gmail.com>
2022-06-19 08:11:47 +0300

    practice 19.06.22

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,21 +1,21 @@
-var unsortedArray = [4,3,2,1]
+var unsortedArray = [4, 3, 2, 1];
 console.log(unsortedArray);
 
-function bubbleSort(arr){
+function bubbleSort(arr) {
   let swapped = true;
-  do{
+  do {
     swapped = false;
-    for(i = 0; i< arr.length; i++){
-      if(arr[i] > arr[i+1]){
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
         let temp = arr[i];
-        arr[i] = arr[i+1];
-        arr[i+1] = temp;
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
         swapped = true;
-        console.log(arr)
+        console.log(arr);
       }
     }
-  } while(swapped)
+  } while (swapped);
   return arr;
 }
 
-bubbleSort(unsortedArray)
+bubbleSort(unsortedArray);
--- quickSort.js
+++ quickSort.js
@@ -3,14 +3,14 @@ function quickSort(arr) {
   let pivot = arr[0];
   const left = [];
   const right = [];
-    
+
   for (let i = 1; i < arr.length; i++) {
     pivot > arr[i] ? left.push(arr[i]) : right.push(arr[i]);
-    }
-  return quickSort(left).concat(pivot, quickSort(right));
   }
-  
+  return quickSort(left).concat(pivot, quickSort(right));
+}
 
-var array = [5,7,2,1,4,9,10,42]
+
+var array = [5, 7, 2, 1, 4, 9, 10, 42]
 
 console.log(quickSort(array))
